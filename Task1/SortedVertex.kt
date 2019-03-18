38c70237920bda65c52144d37d6ac9bc5049e6c0
Kuzivanov Sergey <43182277+SergeyKuz1001@users.noreply.github.com>
2019-03-18 22:07:41 +0300

    Delete SortedVertex.kt

--- Task1/SortedVertex.kt
+++
@@ -1,7 +0,0 @@
-class SortedVertex(val vertex: Int, val length: Int): Comparable<SortedVertex> {
-    override operator fun compareTo(other: SortedVertex): Int {
-        if (this.length == other.length)
-            return this.vertex - other.vertex
-        return this.length - other.length
-    }
-}
