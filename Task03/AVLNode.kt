c9d32ee3a7f37fcf03074709c661aebe43485d77
Kuzivanov Sergey <43182277+SergeyKuz1001@users.noreply.github.com>
2019-04-14 16:50:48 +0300

    Update AVLNode.kt

--- Task03/AVLNode.kt
+++ Task03/AVLNode.kt
@@ -1,24 +1,23 @@
 import kotlin.math.max
-import kotlin.math.abs
 
 class AVLNode<K: Comparable<K>, V>(override var key: K, override var value: V):
     BalancingNode<K, V, AVLNode<K, V>>() {
     override fun createNode(key: K, value: V): AVLNode<K, V> = AVLNode(key, value)
     override fun balancing() {
         updateHeight()
-        if (abs(deltaHeight) != 2)
-            return
-        if (deltaHeight == 2) {
-            if (left!!.deltaHeight == -1)
-                left!!.rotateRightBig()
-            else
-                left!!.rotateRight()
-        }
-        else {
-            if (right!!.deltaHeight == 1)
-                right!!.rotateLeftBig()
-            else
-                right!!.rotateLeft()
+        when (deltaHeight) {
+            -2 -> {
+                if (right!!.deltaHeight == 1)
+                    right!!.rotateBig()
+                else
+                    right!!.rotate()
+            }
+            2 -> {
+                if (left!!.deltaHeight == -1)
+                    left!!.rotateBig()
+                else
+                    left!!.rotate()
+            }
         }
     }
     var height: Int = 1
@@ -32,20 +31,10 @@ class AVLNode<K: Comparable<K>, V>(override var key: K, override var value: V):
         left!!.updateHeight()
         this.updateHeight()
     }
-    fun rotateLeftBig() {
-        val leftSon = left!!
-        leftSon.rotateRight()
-        leftSon.rotateLeft()
-    }
     override fun rotateRight() {
         super.rotateRight()
         right!!.updateHeight()
         this.updateHeight()
-    }
-    fun rotateRightBig() {
-        val rightSon = right!!
-        rightSon.rotateLeft()
-        rightSon.rotateRight()
     }
     override fun toString(): String = "($key to $value, height: $height)"
 }
