aff68e2b4591645b5209369d1c36715b3aca2bf3
Kuzivanov Sergey <43182277+SergeyKuz1001@users.noreply.github.com>
2019-04-14 16:47:13 +0300

    Update BalancingNode.kt

--- Task03/BalancingNode.kt
+++ Task03/BalancingNode.kt
@@ -1,24 +1,41 @@
 abstract class BalancingNode<K: Comparable<K>, V, NT: BalancingNode<K, V, NT>>: Node<K, V, NT>() {
-    override fun createSon(key: K, value: V, typeNewSon: TypeSon) {
-        super.createSon(key, value, typeNewSon)
-        if (typeNewSon == TypeSon.LeftSon)
-            left!!.balancing()
-        else
-            right!!.balancing()
-    }
     abstract fun balancing()
-    open fun rotateLeft() {
+    protected open fun rotateLeft() {
         val father = parent!!
         val leftSon = left
         this.setFather(father.parent, father.type)
         father.setSon(leftSon, TypeSon.RightSon)
         this.setSon(father, TypeSon.LeftSon)
     }
-    open fun rotateRight() {
+    protected open fun rotateRight() {
         val father = parent!!
         val rightSon = right
         this.setFather(father.parent, father.type)
         father.setSon(rightSon, TypeSon.LeftSon)
         this.setSon(father, TypeSon.RightSon)
+    }
+    fun rotate() {
+        when (type) {
+            TypeSon.LeftSon -> rotateRight()
+            TypeSon.RightSon -> rotateLeft()
+            TypeSon.Root -> {}
+        }
+    }
+    private fun rotateLeftBig() {
+        val leftSon = left!!
+        leftSon.rotateRight()
+        leftSon.rotateLeft()
+    }
+    private fun rotateRightBig() {
+        val rightSon = right!!
+        rightSon.rotateLeft()
+        rightSon.rotateRight()
+    }
+    fun rotateBig() {
+        when (type) {
+            TypeSon.LeftSon -> rotateRightBig()
+            TypeSon.RightSon -> rotateLeftBig()
+            TypeSon.Root -> {}
+        }
     }
 }
