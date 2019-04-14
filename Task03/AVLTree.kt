20256ffef569db3acc7b627e14294d89a41fd837
Kuzivanov Sergey <43182277+SergeyKuz1001@users.noreply.github.com>
2019-04-14 16:55:35 +0300

    Update AVLTree.kt

--- Task03/AVLTree.kt
+++ Task03/AVLTree.kt
@@ -1,13 +1,34 @@
 class AVLTree<K: Comparable<K>, V>(): BalancingTree<K, V, AVLNode<K, V>>() {
     constructor(size: Int, init: (Int) -> Pair<K, V>): this() {
-        for (i in 0 until size)
-            set(init(i).first, init(i).second)
+        insert(size, init)
     }
     constructor(root: AVLNode<K, V>): this() {
         this.root = root
     }
     override fun createRoot(key: K, value: V): AVLNode<K, V> = AVLNode(key, value)
-    override fun toString(): String = "AVLTree: {${root!!.subtreeToString()}}"
+    override fun deleteNode(path: MutableList<AVLNode<K, V>>) {
+        val node = path.last()
+        if (node.right == null) {
+            if (node.type == TypeSon.Root)
+                root = node.left
+            node.left?.setFather(node.parent, node.type)
+        }
+        else {
+            var nodeWithNextKey = node.right!!
+            while (nodeWithNextKey.left != null) {
+                path.add(nodeWithNextKey)
+                nodeWithNextKey = nodeWithNextKey.left!!
+            }
+            node.key = nodeWithNextKey.key
+            node.value = nodeWithNextKey.value
+            nodeWithNextKey.parent!!.setSon(nodeWithNextKey.right, nodeWithNextKey.type)
+        }
+        for (nodeOnPath in path.asReversed())
+            nodeOnPath.balancing()
+        while (root!!.type != TypeSon.Root)
+            root = root!!.parent
+    }
+    override fun toString(): String = "AVLTree: {${root?.subtreeToString() ?: ""}}"
 }
 
 fun <K: Comparable<K>, V>avlTreeOf(vararg elements: Pair<K, V>): AVLTree<K, V> =
