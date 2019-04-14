bbe9b86897a31a731d83af9b5cb4218464e36aa0
Kuzivanov Sergey <43182277+SergeyKuz1001@users.noreply.github.com>
2019-04-14 16:53:40 +0300

    Update BalancingTree.kt

--- Task03/BalancingTree.kt
+++ Task03/BalancingTree.kt
@@ -1,8 +1,11 @@
 abstract class BalancingTree<K: Comparable<K>, V, NT: BalancingNode<K, V, NT>>: Tree<K, V, NT>() {
-    override operator fun set(key: K, value: V) {
+    abstract fun deleteNode(path: MutableList<NT>)
+    override fun setWithInfo(key: K, value: V?): InfoAboutNode {
         if (root == null) {
+            if (value == null)
+                return InfoAboutNode.NodeNotFounded
             root = createRoot(key, value)
-            return
+            return InfoAboutNode.NodeCreate
         }
         var node = root
         val path = mutableListOf<NT>()
@@ -10,17 +13,30 @@ abstract class BalancingTree<K: Comparable<K>, V, NT: BalancingNode<K, V, NT>>: 
             path.add(node)
             node = node.nextNode(key)
         }
-        when (path.last().findKey(key)) {
-            TypeSon.Root -> {
-                path.last().value = value
-                return
+        if (value != null) {
+            when (path.last().findKey(key)) {
+                TypeSon.Root -> {
+                    path.last().value = value
+                    return InfoAboutNode.NodeUpdate
+                }
+                TypeSon.LeftSon -> {
+                    path.last().createSon(key, value, TypeSon.LeftSon)
+                    path.add(path.last().left!!)
+                }
+                TypeSon.RightSon -> {
+                    path.last().createSon(key, value, TypeSon.RightSon)
+                    path.add(path.last().right!!)
+                }
             }
-            TypeSon.LeftSon -> path.last().createSon(key, value, TypeSon.LeftSon)
-            TypeSon.RightSon -> path.last().createSon(key, value, TypeSon.RightSon)
+            for (nodeOnPath in path.asReversed())
+                nodeOnPath.balancing()
+            while (root!!.type != TypeSon.Root)
+                root = root!!.parent
+            return InfoAboutNode.NodeCreate
         }
-        for (node in path.asReversed())
-            node.balancing()
-        while (root!!.type != TypeSon.Root)
-            root = root!!.parent
+        if (path.last().key != key)
+            return InfoAboutNode.NodeNotFounded
+        deleteNode(path)
+        return InfoAboutNode.NodeDelete
     }
 }
