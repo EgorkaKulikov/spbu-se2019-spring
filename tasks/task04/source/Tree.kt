d798b9d94fca9b202c4ec5d7bfdb9fa5436a1d3e
lkm30 <lk.malets30@gmail.com>
2019-04-27 18:02:59 +0300

    Fix iterators creation.

--- tasks/task04/source/Tree.kt
+++ tasks/task04/source/Tree.kt
@@ -2,7 +2,7 @@ class Tree<K: Comparable<K>, V>: Iterable<V> {
     private var root: Node<K, V>? = null
 
     override fun iterator() = when(root) {
-        null -> FakeNodeIterator<V>()
-        else -> NodeIterator(root!!)
+        null -> EmptyTreeIterator<V>()
+        else -> TreeIterator(root!!)
     }
 }
