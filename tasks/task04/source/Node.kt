926be463ef435fa345203fa394792fdd787b5721
lkm30 <lk.malets30@gmail.com>
2019-04-28 22:51:15 +0300

    Format code.

--- tasks/task04/source/Node.kt
+++ tasks/task04/source/Node.kt
@@ -1,6 +1,6 @@
 class Node<K: Comparable<K>, V>(
-        var key: K,
-        val value: V,
+        val key: K,
+        var value: V,
         var parent: Node<K, V>?
 ) {
     var left: Node<K, V>? = null
