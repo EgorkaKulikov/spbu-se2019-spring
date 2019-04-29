0ae984bb62a3a70a33dc414846e00fa3ffdfb7f6
Kuzivanov Sergey <43182277+SergeyKuz1001@users.noreply.github.com>
2019-04-29 15:52:46 +0300

    Update AVLNode.kt

--- Task03/AVLNode.kt
+++ Task03/AVLNode.kt
@@ -1,40 +1,49 @@
 import kotlin.math.max
 
-class AVLNode<K: Comparable<K>, V>(override var key: K, override var value: V):
-    BalancingNode<K, V, AVLNode<K, V>>() {
-    override fun createNode(key: K, value: V): AVLNode<K, V> = AVLNode(key, value)
-    override fun balancing() {
+class AVLNode<K: Comparable<K>, V>(override val key: K, override var value: V):
+    NodeWithBalancing<K, V, AVLNode<K, V>>() {
+    protected override fun createNode(key: K, value: V): AVLNode<K, V> = AVLNode(key, value)
+    public override fun balancing() {
         updateHeight()
+        // перебалансировка происходит тогда и только тогда, когда
+        // разница между высотами левого и правого поддерева = 2
         when (deltaHeight) {
             -2 -> {
                 if (right!!.deltaHeight == 1)
-                    right!!.rotateBig()
+                    right!!.bigRotate()
                 else
                     right!!.rotate()
             }
             2 -> {
                 if (left!!.deltaHeight == -1)
-                    left!!.rotateBig()
+                    left!!.bigRotate()
                 else
                     left!!.rotate()
             }
         }
     }
     var height: Int = 1
-    fun updateHeight() {
+        private set(value) { field = value }
+    private fun updateHeight() {
         height = max(left?.height ?: 0, right?.height ?: 0) + 1
     }
-    val deltaHeight: Int
+    private val deltaHeight: Int
         get() = (left?.height ?: 0) - (right?.height ?: 0)
-    override fun rotateLeft() {
+    protected override fun rotateLeft() {
         super.rotateLeft()
         left!!.updateHeight()
         this.updateHeight()
     }
-    override fun rotateRight() {
+    protected override fun rotateRight() {
         super.rotateRight()
         right!!.updateHeight()
         this.updateHeight()
     }
-    override fun toString(): String = "($key to $value, height: $height)"
+    public override fun copy() = AVLNode(key, value)
+    override fun equals(other: Any?): Boolean =
+        (other is AVLNode<*, *> &&
+                other.left == left &&
+                other.right == right &&
+                other.key == key &&
+                other.value == value)
 }
