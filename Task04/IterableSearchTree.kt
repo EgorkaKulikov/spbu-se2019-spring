453a9a0225b3211b9ba566e750924bdab3ac2dd2
Dmitry Kanashin <dmitrii.a.kanashin@gmail.com>
2019-05-04 18:45:29 +0300

    Added solution for task04

---
+++ Task04/IterableSearchTree.kt
@@ -0,0 +1,117 @@
+class IterableSearchTree<T, K: Comparable<K>>: Iterable<T> {
+    inner class Node(var value : T, var key : K, var parent : Node?) {
+        var left : Node? = null
+        var right : Node? = null
+    }
+
+    private var root : Node? = null
+
+    private fun createNode(value : T, key: K, parent: Node?) : Node {
+        return Node(value, key, parent)
+    }
+
+    fun find(key: K): T? {
+        var currNode = this.root
+
+        while(currNode != null) {
+            when {
+                currNode.key == key -> return currNode.value
+                currNode.key > key -> currNode = currNode.left
+                currNode.key < key -> currNode = currNode.right
+            }
+        }
+
+        return null
+    }
+
+    fun insert(value : T, key: K): Node{
+        var currNode = this.root
+
+        if (currNode == null) {
+            currNode = createNode(value, key, null)
+            return currNode
+        }
+
+        while (currNode!!.key != key) {
+            when {
+                currNode.key > key -> {
+                    if (currNode.left == null) {
+                        val insertedNode = createNode(value, key, currNode)
+                        currNode.left = insertedNode
+                        return insertedNode
+                    }
+                    else {
+                        currNode = currNode.left
+                    }
+                }
+                currNode.key < key -> {
+                    if (currNode.right == null) {
+                        val insertedNode = createNode(value, key, currNode)
+                        currNode.right = insertedNode
+                        return insertedNode
+                    }
+                    else {
+                        currNode = currNode.right
+                    }
+                }
+            }
+        }
+
+        currNode.value = value
+        return currNode
+
+    }
+    private fun findMinNode(startNode: Node?): Node?{
+        var currLeftNode = startNode
+        while (currLeftNode?.left != null){
+            currLeftNode = currLeftNode.left
+        }
+        return currLeftNode
+    }
+
+    // Infix-traverse
+    inner class SearchTreeIterator : Iterator<T>{
+
+        // We find first needed node - node with min key
+        private var currNode = findMinNode(root)
+
+        // So we will find next node and return previous node's value
+        override fun next(): T {
+
+            var nextNode = currNode
+            // We sure about safe currNode because it checks in hasNext()
+            // Even if we have empty tree findMinNode(root) will return
+            // null to the first node and it will be checked in hasNext()
+            val value = currNode!!.value
+
+            if (nextNode?.right != null) {
+                nextNode = findMinNode(nextNode.right)
+            } else {
+                while(nextNode?.parent != null){
+                    if (nextNode.parent?.right == nextNode){
+                        nextNode = nextNode.parent
+                    } else {
+                        break
+                    }
+                }
+                if (nextNode?.parent != null){
+                    nextNode = nextNode.parent
+                } else {
+                    nextNode = null
+                }
+            }
+
+            currNode = nextNode
+            return value
+        }
+
+        override fun hasNext(): Boolean {
+            return currNode != null
+        }
+    }
+
+    // Returns object of iterator
+    override fun iterator(): Iterator<T>{
+        return SearchTreeIterator()
+    }
+}
