c27c6a5cffb3ac9b205e1bf033cfe9642aabb7fa
Tsirikov <tsirikov.s.a@gmail.com>
2019-05-04 14:34:20 +0300

    Convert indentations to spaces.

--- Task05/model.kt
+++ Task05/model.kt
@@ -6,140 +6,140 @@ import kotlin.system.exitProcess
 
 
 /*
-	Weird constants are from .ZIP File Format Specification.
-	https://pkware.cachefly.net/webdocs/casestudies/APPNOTE.TXT
+  Weird constants are from .ZIP File Format Specification.
+  https://pkware.cachefly.net/webdocs/casestudies/APPNOTE.TXT
 */
 data class Zipfile(val zipname: String)
-{	
-	public fun name() = filename
-	
-	public fun time() = time
+{ 
+  public fun name() = filename
+  
+  public fun time() = time
 
-	public fun date() = date
+  public fun date() = date
 
-	public fun filesize() = filesize
+  public fun filesize() = filesize
 
-	public fun hasNext() : Boolean
-	{
-		if (size() >= 22 
-			&& bitHasNext == true 
-			&& bitNext == false)
-		{
-			bitNext = true
+  public fun hasNext() : Boolean
+  {
+    if (size() >= 22 
+      && bitHasNext == true 
+      && bitNext == false)
+    {
+      bitNext = true
 
-			when(bytesToLong(4))
-			{
-				0x04034B50L -> bitHasNext = true
-				
-				0x02014B50L,
-				0x05064B50L -> bitHasNext = false
-				
-				else ->
-				{
-					println("Incorrect data.")
-					exitProcess(3)
-				}
-			}
+      when(bytesToLong(4))
+      {
+        0x04034B50L -> bitHasNext = true
+        
+        0x02014B50L,
+        0x05064B50L -> bitHasNext = false
+        
+        else ->
+        {
+          println("Incorrect data.")
+          exitProcess(3)
+        }
+      }
 
-			return bitHasNext
-		}
-		else if (bitNext == true)
-		{
-			return bitHasNext
-		}
-		else
-		{
-				println("Incorrect size of data.")
-				exitProcess(3)
-		}
-	}
+      return bitHasNext
+    }
+    else if (bitNext == true)
+    {
+      return bitHasNext
+    }
+    else
+    {
+        println("Incorrect size of data.")
+        exitProcess(3)
+    }
+  }
 
-	public fun next()
-	{
-		var offset = 0L
-		
-		zip.skip(2L)
-	
-		if (bytesToLong(1).toUByte().toInt() and 8 == 8)
-		{
-			offset += 16L
-		}
-	
-		zip.skip(3L)
-		
-		time = bytesToLong(2)
-		date = bytesToLong(2)
-		
-		zip.skip(4L)
-		
-		
-		offset += bytesToLong(4)
-			
-		filesize = bytesToLong(4)
-		
-		val nameOffset = bytesToLong(2).toInt()
-		offset += bytesToLong(2)
-		filename = bytesToString(nameOffset)
-		
-		if (zip.skip(offset) == -1L)
-		{
-			println("Error of positioning.")
-			exitProcess(4)
-		}
+  public fun next()
+  {
+    var offset = 0L
+    
+    zip.skip(2L)
+  
+    if (bytesToLong(1).toUByte().toInt() and 8 == 8)
+    {
+      offset += 16L
+    }
+  
+    zip.skip(3L)
+    
+    time = bytesToLong(2)
+    date = bytesToLong(2)
+    
+    zip.skip(4L)
+    
+    
+    offset += bytesToLong(4)
+      
+    filesize = bytesToLong(4)
+    
+    val nameOffset = bytesToLong(2).toInt()
+    offset += bytesToLong(2)
+    filename = bytesToString(nameOffset)
+    
+    if (zip.skip(offset) == -1L)
+    {
+      println("Error of positioning.")
+      exitProcess(4)
+    }
 
-		bitNext = false
-	}
+    bitNext = false
+  }
 
 
-	private val zip = File(zipname).inputStream()
+  private val zip = File(zipname).inputStream()
 
-	private fun size() = zip.available()
+  private fun size() = zip.available()
 
-	private var filename = ""
+  private var filename = ""
 
-	private var time = 0L
+  private var time = 0L
 
-	private var date = 0L
+  private var date = 0L
 
-	private var filesize = 0L
+  private var filesize = 0L
 
-	private var bitHasNext = true
+  private var bitHasNext = true
 
-	private var bitNext = false
+  private var bitNext = false
 
-	private fun bytesToLong(size: Int) : Long
-	{
-		val field = extract(size)
-		var longField = 0L
-		
-		for (i in size-1 downTo 0)
-		{
-			longField *= 256L 
-			longField += field[i].toUByte().toLong()
-		}
+  private fun bytesToLong(size: Int) : Long
+  {
+    val field = extract(size)
+    var longField = 0L
+    
+    for (i in size-1 downTo 0)
+    {
+      longField *= 256L 
+      longField += field[i].toUByte().toLong()
+    }
 
-		return longField
-	}
+    return longField
+  }
 
-	private	val charset = Charsets.UTF_8
+  private val charset = Charsets.UTF_8
 
-	private fun bytesToString(size: Int) : String
-	{
-		return extract(size).toString(charset)
-	}
+  private fun bytesToString(size: Int) : String
+  {
+    return extract(size).toString(charset)
+  }
 
-	private fun extract(size: Int) : ByteArray
-	{
-		val record = ByteArray(size)
-		
-		if (zip.read(record) == size)
-		{
-			return record
-		}
-		else
-		{
-			println("Error of reading.")
-			exitProcess(4)
-		}		
-	}
+  private fun extract(size: Int) : ByteArray
+  {
+    val record = ByteArray(size)
+    
+    if (zip.read(record) == size)
+    {
+      return record
+    }
+    else
+    {
+      println("Error of reading.")
+      exitProcess(4)
+    }   
+  }
 }
--- Task05/view.kt
+++ Task05/view.kt
@@ -2,8 +2,8 @@ package zipviewer
 
 
 /*
-	"All slashes MUST be forward slashes '/'"
-	4.4.17.1 of .ZIP File Format Specification
+  "All slashes MUST be forward slashes '/'"
+  4.4.17.1 of .ZIP File Format Specification
 */
 const val slash = '/'
 const val indent = "    "
@@ -11,102 +11,102 @@ const val indent = "    "
 
 fun tree(zip: Zipfile)
 {
-	var dirname = ""
-	var offset = 0
+  var dirname = ""
+  var offset = 0
 
-	while (zip.hasNext() == true)
-	{
-		zip.next()
-		val filename = zip.name()
+  while (zip.hasNext() == true)
+  {
+    zip.next()
+    val filename = zip.name()
 
-		while (filename.startsWith(dirname) == false)
-		{
-			offset--
+    while (filename.startsWith(dirname) == false)
+    {
+      offset--
 
-			if (dirname.last().equals(slash))
-			{
-				dirname = dirname.dropLast(1)
-			}
-			
-			dirname = dirname.dropLastWhile{!it.equals(slash)}
-		}
-		
-		print(indent.repeat(offset))
-		println(filename.drop(dirname.length))
+      if (dirname.last().equals(slash))
+      {
+        dirname = dirname.dropLast(1)
+      }
+      
+      dirname = dirname.dropLastWhile{!it.equals(slash)}
+    }
+    
+    print(indent.repeat(offset))
+    println(filename.drop(dirname.length))
 
-		if (filename.endsWith(slash))
-		{
-			offset++
-			dirname = filename
-		}
-	}
+    if (filename.endsWith(slash))
+    {
+      offset++
+      dirname = filename
+    }
+  }
 }
 
 
 fun size(zip: Zipfile, name: String)
 {
-	var summarySize = 0L
-	var existing = false
-	
-	while (zip.hasNext() == true)
-	{
-		zip.next()
-		val filename = zip.name()
-		
-		if (filename.startsWith(name.plus(slash)) 
-			|| filename.contains(slash.plus(name).plus(slash)))
-		{
-			existing = true
-			summarySize += zip.filesize()
-		}	
-	}
+  var summarySize = 0L
+  var existing = false
+  
+  while (zip.hasNext() == true)
+  {
+    zip.next()
+    val filename = zip.name()
+    
+    if (filename.startsWith(name.plus(slash)) 
+      || filename.contains(slash.plus(name).plus(slash)))
+    {
+      existing = true
+      summarySize += zip.filesize()
+    } 
+  }
 
-	if (existing == true)
-	{
-		println("Size of directory '$name' is $summarySize B.")
-	}
-	else
-	{
-		println("Directory '$name' doesn`t exist.")
-	}
+  if (existing == true)
+  {
+    println("Size of directory '$name' is $summarySize B.")
+  }
+  else
+  {
+    println("Directory '$name' doesn`t exist.")
+  }
 }
 
 
 /*
-	Using MS-DOS date & time format.
-	http://www.vsft.com/hal/dostime.htm
+  Using MS-DOS date & time format.
+  http://www.vsft.com/hal/dostime.htm
 */
 fun time(zip: Zipfile, name: String)
 {
-	var existing = false
+  var existing = false
 
-	while (zip.hasNext())
-	{
-		zip.next()
-		val filename = zip.name()
-		
-		if (filename.endsWith(slash.plus(name)) 
-			|| filename.contentEquals(name))
-		{
-			existing = true
-			val date = zip.date()
-			val time = zip.time()
-			
-			val year = 1980 + (date and 0xFE00) / 0x200
-			val month = (date and 0x1E0) / 0x20
-			val day = (date and 0x1F)
-			
-			val hour = time / 0x800
-			val minute = (time and 0x7E0) / 0x20
-			val second = (time and 0x1F) * 2
-			
-			print("'$filename' was lastly edited ")
-			println("$day.$month.$year at $hour:$minute:$second.")
-		}
-	}
+  while (zip.hasNext())
+  {
+    zip.next()
+    val filename = zip.name()
+    
+    if (filename.endsWith(slash.plus(name)) 
+      || filename.contentEquals(name))
+    {
+      existing = true
+      val date = zip.date()
+      val time = zip.time()
+      
+      val year = 1980 + (date and 0xFE00) / 0x200
+      val month = (date and 0x1E0) / 0x20
+      val day = (date and 0x1F)
+      
+      val hour = time / 0x800
+      val minute = (time and 0x7E0) / 0x20
+      val second = (time and 0x1F) * 2
+      
+      print("'$filename' was lastly edited ")
+      println("$day.$month.$year at $hour:$minute:$second.")
+    }
+  }
 
-	if (existing == false)
-	{
-		println("No such file or directory.")
-	}
+  if (existing == false)
+  {
+    println("No such file or directory.")
+  }
 }
