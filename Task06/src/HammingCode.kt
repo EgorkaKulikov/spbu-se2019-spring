844973f497e6e0114e9faf78c1f431217a554ea3
Vadim Abzalov <vadim.i.abzalov@gmail.com>
2019-05-05 21:59:29 +0300

    Added Task06

---
+++ Task06/src/HammingCode.kt
@@ -0,0 +1,179 @@
+import java.util.*
+
+class HammingCode(private val data: ByteArray = ByteArray(0) { it.toByte() }, val codeLen: Int = 2) {
+
+    inner class WordHammingCode(private val word: ByteArray, private val wordLen: Int = codeLen) {
+
+        private var controlBitsPlaces = Array(getControlBitsCount() + 1) { i -> (1 shl (i - 1)) }
+        private var controlBits = BitSet()
+        private var encodedWord = BitSet()
+        private val encodedWordLen = wordLen * 8 + controlBitsPlaces.size
+        private var controlSequences = Array(controlBitsPlaces.size) { i -> buildControlSequence(i) }
+
+        private fun getControlBitsCount(): Int {
+
+            var res = 0
+            var cur = 1
+
+            while (cur <= wordLen * 8) {
+                ++res
+                cur = cur shl 1
+            }
+
+            return res
+
+        }
+
+        private fun buildControlSequence(cnt: Int): BitSet {
+
+            val res = BitSet()
+
+            if (cnt == 0) return res
+
+            for (i in (1 shl (cnt - 1)) until encodedWordLen step (1 shl cnt)) {
+                for (j in 1..(1 shl (cnt - 1))) {
+                    res.set(i + j - 1)
+                }
+            }
+
+            return res
+
+        }
+
+        fun encodeWord() {
+
+            val bitWord = word.toBitSet()
+
+            var bitWordPointer = 0
+
+            for (i in 1 until encodedWordLen) {
+                if (i and (i - 1) != 0) {
+                    if (bitWord.get(bitWordPointer)) {
+                        encodedWord.set(i)
+                    }
+                    ++bitWordPointer
+                }
+            }
+
+            for (i in 1..controlBitsPlaces.lastIndex) {
+
+                val cur = BitSet()
+
+                cur.or(controlSequences[i])
+
+                cur.and(encodedWord)
+
+                if (cur.count(true) % 2 == 1) {
+                    encodedWord.set(controlBitsPlaces[i])
+                    controlBits.set(i)
+                }
+
+            }
+
+        }
+
+        fun decodeWord(wordToDecode: ByteArray): ByteArray {
+
+            val bitDecodedWord = BitSet()
+
+            val bitWord = wordToDecode.toBitSet()
+
+            var bitWordPointer = 0
+
+            for (i in 1 until encodedWordLen) {
+                if (i and (i - 1) != 0) {
+                    if (bitWord.get(bitWordPointer)) {
+                        bitDecodedWord.set(i)
+                    }
+                    ++bitWordPointer
+                }
+            }
+
+            for (i in 1..controlBitsPlaces.lastIndex) {
+                if (controlBits.get(i)) {
+                    bitDecodedWord.set(controlBitsPlaces[i])
+                }
+            }
+
+            var errorPosition = 0
+
+            for (i in 1..controlBitsPlaces.lastIndex) {
+
+                val cur = BitSet()
+
+                cur.or(controlSequences[i])
+
+                cur.and(bitDecodedWord)
+
+                if (cur.count(true) % 2 == 1) {
+                    errorPosition += (1 shl (i - 1))
+                }
+
+            }
+
+            bitDecodedWord.flip(errorPosition)
+
+            var decodedWordPointer = 0
+
+            val decodedWord = BitSet()
+
+            for (i in 1 until encodedWordLen) {
+                if (i and (i - 1) != 0) {
+                    if (bitDecodedWord.get(i)) {
+                        decodedWord.set(decodedWordPointer)
+                    }
+                    ++decodedWordPointer
+                }
+            }
+
+            return decodedWord.toByteArray(wordLen)
+
+        }
+
+    }
+
+    private var encodedWords = Array(0) { _ -> WordHammingCode(ByteArray(0) { it.toByte() }) }
+
+    fun encode() {
+
+        for (i in data.indices step codeLen) {
+
+            var curByteArray = ByteArray(0)
+
+            for (j in 1..codeLen) {
+                curByteArray += data[i + j - 1].toByteArray()
+            }
+
+            val curWord = WordHammingCode(curByteArray)
+
+            curWord.encodeWord()
+
+            encodedWords += curWord
+
+        }
+
+    }
+
+    fun decode(dataToDecode: ByteArray): ByteArray {
+
+        var res = ByteArray(0)
+
+        for ((cnt, i) in (dataToDecode.indices step codeLen).withIndex()) {
+
+            var curByteArray = ByteArray(0)
+
+            for (j in 1..codeLen) {
+                curByteArray += dataToDecode[i + j - 1].toByteArray()
+            }
+
+            val decodedWord = encodedWords[cnt].decodeWord(curByteArray)
+
+            res += decodedWord
+
+        }
+
+        return res
+
+    }
+
+}
---
+++ Task06/src/main.kt
@@ -0,0 +1,149 @@
+import java.io.File
+
+import java.util.*
+
+fun BitSet.count(type: Boolean): Int {
+
+    var res = 0
+
+    for (i in 0..this.length()) {
+        if (this.get(i) == type) {
+            ++res
+        }
+    }
+
+    return res
+
+}
+
+fun BitSet.toByteArray(cnt: Int): ByteArray {
+
+    var res = ByteArray(0)
+
+    for (i in 0 until cnt) {
+
+        var cur = 0
+
+        for (j in 0..7) {
+            if (this.get(8 * i + j)) {
+                cur += (1 shl j)
+            }
+        }
+
+        res += cur.toByte().toByteArray()
+
+    }
+
+    return res
+
+}
+
+fun Byte.toBitSet(): BitSet {
+
+    var cur = this.toLong()
+
+    val res = BitSet()
+
+    for (i in 0..7) {
+        if (cur % 2 == 1L) {
+            res.set(i, true)
+        } else {
+            res.set(i, false)
+        }
+        cur = cur ushr 1
+    }
+
+    return res
+
+}
+
+fun Byte.toByteArray(): ByteArray {
+
+    val res = ByteArray(1)
+
+    res[0] = this
+
+    return res
+
+}
+
+fun ByteArray.toBitSet(): BitSet {
+
+    val res = BitSet()
+
+    for (i in this.indices) {
+        val cur = this[i].toBitSet()
+        for (j in 0..7) {
+            if (cur.get(j)) {
+                res.set(8 * i + j, true)
+            } else {
+                res.set(8 * i + j, false)
+            }
+        }
+    }
+
+    return res
+
+}
+
+fun addSalt(data: ByteArray, percent: Int): ByteArray {
+
+    val res = ByteArray(data.size) { i -> data[i] }
+
+    for (i in 1..(res.size * percent / 100)) {
+        val pos = (res.indices).random()
+        val curBitSet = res[pos].toBitSet()
+        val bit = (0..7).random()
+        curBitSet.flip(bit)
+        res[pos] = curBitSet.toByteArray(1)[0]
+    }
+
+    return res
+
+}
+
+fun main() {
+
+    val fileName = "src/Picture.bmp"
+    val file = File(fileName)
+    val input: ByteArray = file.readBytes()
+    val headerSize = 54
+
+    var headerData = ByteArray(0)
+
+    for (i in 0 until headerSize) {
+        headerData += input[i]
+    }
+
+    val dataSize = input.size - headerSize
+    val data = ByteArray(dataSize)
+
+    for (i in headerSize until input.size)
+        data[i - headerSize] = input[i]
+
+    val hammingCode = HammingCode(data)
+    hammingCode.encode()
+
+    for (percent in 5..55 step 10) {
+
+        val saltedData = addSalt(data, percent)
+
+        val newFileNameSalted = "src/PictureSalted$percent%.bmp"
+        val newFileSalted = File(newFileNameSalted)
+        newFileSalted.createNewFile()
+        var resultSalted = headerData
+        resultSalted += saltedData
+        newFileSalted.writeBytes(resultSalted)
+
+        val decodedData = hammingCode.decode(saltedData)
+
+        val newFileNameDecoded = "src/PictureDecoded$percent%.bmp"
+        val newFileDecoded = File(newFileNameDecoded)
+        newFileDecoded.createNewFile()
+        var resultDecoded = headerData
+        resultDecoded += decodedData
+        newFileDecoded.writeBytes(resultDecoded)
+
+    }
+
+}
