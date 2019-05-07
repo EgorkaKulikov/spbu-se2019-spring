65d69f9038a8f2c2901e8b57f2bf487b21b388ab
Kuzivanov Sergey <43182277+SergeyKuz1001@users.noreply.github.com>
2019-05-07 19:37:59 +0300

    Update BinarySearchNode.kt

--- Task03/BinarySearchNode.kt
+++ Task03/BinarySearchNode.kt
@@ -1,7 +1,9 @@
 class BinarySearchNode<K: Comparable<K>, V>(override val key: K, override var value: V):
     Node<K, V, BinarySearchNode<K, V>>() {
     protected override fun createNode(key: K, value: V): BinarySearchNode<K, V> = BinarySearchNode(key, value)
+
     public override fun copy() = BinarySearchNode(key, value)
+
     override fun equals(other: Any?): Boolean =
         (other is BinarySearchNode<*, *> &&
                 other.left == left &&
