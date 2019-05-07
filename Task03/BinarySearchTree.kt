22c71fbace1f508bd0ced9b1f7926bbe9295318a
Kuzivanov Sergey <43182277+SergeyKuz1001@users.noreply.github.com>
2019-05-07 19:38:19 +0300

    Update BinarySearchTree.kt

--- Task03/BinarySearchTree.kt
+++ Task03/BinarySearchTree.kt
@@ -2,10 +2,13 @@ class BinarySearchTree<K: Comparable<K>, V>(): Tree<K, V, BinarySearchNode<K, V>
     constructor(size: Int, init: (Int) -> Pair<K, V>): this() {
         insert(size, init)
     }
+
     constructor(root: BinarySearchNode<K, V>): this() {
         this.root = root
     }
+
     protected override fun createRoot(key: K, value: V): BinarySearchNode<K, V> = BinarySearchNode(key, value)
+
     override fun equals(other: Any?): Boolean =
         (other is BinarySearchTree<*, *> && other.root == root)
 }
