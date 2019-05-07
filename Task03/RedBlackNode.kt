b953d6308be89de448c4b7229015e3117567fe8b
Kuzivanov Sergey <43182277+SergeyKuz1001@users.noreply.github.com>
2019-05-07 19:41:00 +0300

    Update RedBlackNode.kt

--- Task03/RedBlackNode.kt
+++ Task03/RedBlackNode.kt
@@ -4,8 +4,9 @@ enum class Color {
 }
 
 class RedBlackNode<K: Comparable<K>, V>(override val key: K, override var value: V, var color: Color):
-    NodeWithBalancing<K, V, RedBlackNode<K, V>>() {
+    BalanceNode<K, V, RedBlackNode<K, V>>() {
     protected override fun createNode(key: K, value: V): RedBlackNode<K, V> = RedBlackNode(key, value, Color.Red)
+
     public override fun balancing() {
         if (color == Color.Black || parent!!.color == Color.Black)
             return
@@ -21,12 +22,13 @@ class RedBlackNode<K: Comparable<K>, V>(override val key: K, override var value:
         else {
             father.color = Color.Black
             uncle.color = Color.Black
-            if (grandfather.type != TypeSon.Root)
+            if (grandfather.type != SonType.Root)
                 grandfather.color = Color.Red
         }
     }
-    protected override fun rotateLeft() {
-        super.rotateLeft()
+
+    public override fun leftRotate() {
+        super.leftRotate()
         if (left!!.color == Color.Red) {
             left!!.color = this.color
             this.color = Color.Red
@@ -36,8 +38,9 @@ class RedBlackNode<K: Comparable<K>, V>(override val key: K, override var value:
             this.color = Color.Black
         }
     }
-    protected override fun rotateRight() {
-        super.rotateRight()
+
+    public override fun rightRotate() {
+        super.rightRotate()
         if (right!!.color == Color.Red) {
             right!!.color = this.color
             this.color = Color.Red
@@ -47,7 +50,19 @@ class RedBlackNode<K: Comparable<K>, V>(override val key: K, override var value:
             this.color = Color.Black
         }
     }
+
     public override fun copy() = RedBlackNode(key, value, color)
+
+    public fun addLeftSon(key: K, value: V, color: Color) {
+        super.addLeftSon(key, value)
+        left!!.color = color
+    }
+
+    public fun addRightSon(key: K, value: V, color: Color) {
+        super.addRightSon(key, value)
+        right!!.color = color
+    }
+
     override fun equals(other: Any?): Boolean =
         (other is RedBlackNode<*, *> &&
                 other.left == left &&
