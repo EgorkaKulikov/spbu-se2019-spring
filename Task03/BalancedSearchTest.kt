546f8d0840bf933510748a9a03cc7ae5b518c5b0
Dmitry Kanashin <dmitrii.a.kanashin@gmail.com>
2019-05-09 19:39:16 +0300

    Added Task03 solution

---
+++ Task03/BalancedSearchTest.kt
@@ -0,0 +1,174 @@
+package trees
+
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+
+class BalancedSearchTreeTest {
+    @Test
+    fun rotateLeftBasicTest() {
+        val tree = RBTree<Int, Int>()
+        val node1 = tree.RBNode(0, 0, null)
+        val node2 = tree.RBNode(0, 0, node1)
+
+        node1.right = node2
+
+        node1.rotateLeft()
+        Assertions.assertEquals(node2.left, node1)
+        Assertions.assertEquals(node1.parent, node2)
+    }
+
+    @Test
+    fun rotateRightBasicTest() {
+        val tree = RBTree<Int, Int>()
+        val node2 = tree.RBNode(0, 0, null)
+        val node1 = tree.RBNode(0, 0, node2)
+
+        node2.left = node1
+
+        node2.rotateRight()
+        Assertions.assertEquals(node1.right, node2)
+        Assertions.assertEquals(node2.parent, node1)
+    }
+
+    @Test
+    fun rotateLeftTestIsLeftChild() {
+        val tree = RBTree<Int, Int>()
+        val parent = tree.RBNode(0, 0, null)
+        val node1 = tree.RBNode(0, 0, parent)
+        val node2 = tree.RBNode(0, 0, node1)
+
+        node1.right = node2
+        parent.left = node1
+
+        node1.rotateLeft()
+        Assertions.assertEquals(node2.left, node1)
+        Assertions.assertEquals(node1.parent, node2)
+        Assertions.assertEquals(parent.left, node2)
+        Assertions.assertEquals(parent, node2.parent)
+    }
+
+    @Test
+    fun rotateRightTestIsLeftChild() {
+        val tree = RBTree<Int, Int>()
+        val parent = tree.RBNode(0, 0, null)
+        val node2 = tree.RBNode(0, 0, parent)
+        val node1 = tree.RBNode(0, 0, node2)
+
+        node2.left = node1
+        parent.left = node2
+
+        node2.rotateRight()
+        Assertions.assertEquals(node1.right, node2)
+        Assertions.assertEquals(node2.parent, node1)
+        Assertions.assertEquals(parent.left, node1)
+        Assertions.assertEquals(parent, node1.parent)
+    }
+
+    @Test
+    fun rotateLeftTestIsRightChild() {
+        val tree = RBTree<Int, Int>()
+        val parent = tree.RBNode(0, 0, null)
+        val node1 = tree.RBNode(0, 0, parent)
+        val node2 = tree.RBNode(0, 0, node1)
+
+        node1.right = node2
+        parent.right = node1
+
+        node1.rotateLeft()
+        Assertions.assertEquals(node2.left, node1)
+        Assertions.assertEquals(node1.parent, node2)
+        Assertions.assertEquals(parent.right, node2)
+        Assertions.assertEquals(parent, node2.parent)
+    }
+
+    @Test
+    fun rotateRightTestIsRightChild() {
+        val tree = RBTree<Int, Int>()
+        val parent = tree.RBNode(0, 0, null)
+        val node2 = tree.RBNode(0, 0, parent)
+        val node1 = tree.RBNode(0, 0, node2)
+
+        node2.left = node1
+        parent.right = node2
+
+        node2.rotateRight()
+        Assertions.assertEquals(node1.right, node2)
+        Assertions.assertEquals(node2.parent, node1)
+        Assertions.assertEquals(parent.right, node1)
+        Assertions.assertEquals(parent, node1.parent)
+    }
+
+    @Test
+    fun rotateLeftTestHasLeftChild() {
+        val tree = RBTree<Int, Int>()
+        val node1 = tree.RBNode(0, 0, null)
+        val node2 = tree.RBNode(0, 0, node1)
+        val leftChild = tree.RBNode(0, 0, node1)
+
+        node1.right = node2
+        node1.left = leftChild
+
+        node1.rotateLeft()
+        Assertions.assertEquals(node2.left, node1)
+        Assertions.assertEquals(node1.parent, node2)
+        Assertions.assertEquals(leftChild, node1.left)
+        Assertions.assertEquals(leftChild.parent, node1)
+    }
+
+    @Test
+    fun rotateRightTestHasRightChild() {
+        val tree = RBTree<Int, Int>()
+        val node2 = tree.RBNode(0, 0, null)
+        val node1 = tree.RBNode(0, 0, node2)
+        val rightChild = tree.RBNode(0, 0, node2)
+
+        node2.right = rightChild
+        node2.left = node1
+
+        node2.rotateRight()
+        Assertions.assertEquals(node1.right, node2)
+        Assertions.assertEquals(node2.parent, node1)
+        Assertions.assertEquals(rightChild, node2.right)
+        Assertions.assertEquals(rightChild.parent, node2)
+    }
+
+    @Test
+    fun rotateLeftTestHasGrandchildren() {
+        val tree = RBTree<Int, Int>()
+        val node1 = tree.RBNode(0, 0, null)
+        val node2 = tree.RBNode(0, 0, node1)
+        val grandChildLeft = tree.RBNode(0, 0, node2)
+        val grandChildRight = tree.RBNode(0, 0, node2)
+
+        node1.right = node2
+        node2.right = grandChildRight
+        node2.left = grandChildLeft
+
+        node1.rotateLeft()
+        Assertions.assertEquals(node2.left, node1)
+        Assertions.assertEquals(node1.parent, node2)
+        Assertions.assertEquals(grandChildRight, node2.right)
+        Assertions.assertEquals(grandChildLeft, node1.right)
+    }
+
+    @Test
+    fun rotateRightTestHasGrandchildren() {
+        val tree = RBTree<Int, Int>()
+        val node2 = tree.RBNode(0, 0, null)
+        val node1 = tree.RBNode(0, 0, node2)
+        val grandChildLeft = tree.RBNode(0, 0, node2)
+        val grandChildRight = tree.RBNode(0, 0, node2)
+
+        node2.left = node1
+        node1.right = grandChildRight
+        node1.left = grandChildLeft
+
+        node2.rotateRight()
+        Assertions.assertEquals(node1.right, node2)
+        Assertions.assertEquals(node2.parent, node1)
+        Assertions.assertEquals(grandChildLeft, node1.left)
+        Assertions.assertEquals(grandChildRight, node2.left)
+    }
+
+
+}
---
+++ Task03/BinaryTreeBalanced.kt
@@ -0,0 +1,100 @@
+package trees
+
+abstract class BalancedSearchTree<K : Comparable<K>, T> : BinarySearchTree<K, T>() {
+
+    open inner class BalancedNode(key_: K, value_: T, parent_: Node?) : Node(key_, value_, parent_) {
+        internal fun getGrandparent(): Node? = this.parent?.parent
+
+        internal fun getUncle(): Node? {
+            val grandparent = this.parent?.parent
+            return when {
+                grandparent == null -> null
+                grandparent?.left == this.parent -> grandparent?.right
+                else -> grandparent?.left
+            }
+        }
+
+        internal fun getSibling(): Node? {
+            val parent = this.parent
+            return if (parent?.left == this) {
+                parent?.right
+            } else {
+                parent?.left
+            }
+        }
+
+        fun rotateLeft() {
+            if (this.right == null) {
+                return
+            }
+            val rightNode = this.right
+
+            if (rightNode?.left != null) {
+                rightNode.left!!.parent = this
+            }
+            this.right = rightNode?.left
+
+            val parentOfCurrNode: Node? = this.parent
+            if (parentOfCurrNode == null){
+                root = rightNode
+            } else {
+                if (parentOfCurrNode?.left == this){
+                    parentOfCurrNode.left = rightNode
+                } else {
+                    parentOfCurrNode!!.right = rightNode
+                }
+            }
+
+            rightNode?.parent = parentOfCurrNode
+            this.parent = rightNode
+
+            rightNode!!.left = this
+        }
+
+        fun rotateRight() {
+            if (this.left == null) {
+                return
+            }
+
+            val leftNode = this.left
+
+            if (leftNode?.right != null) {
+                leftNode.right!!.parent = this
+            }
+            this.left = leftNode?.right
+
+            val parentOfCurrNode: Node? = this.parent
+            if (parentOfCurrNode == null){
+                root = leftNode
+            } else {
+                if (parentOfCurrNode?.left == this){
+                    parentOfCurrNode.left = leftNode
+                } else {
+                    parentOfCurrNode!!.right = leftNode
+                }
+            }
+
+            leftNode?.parent = parentOfCurrNode
+            this.parent = leftNode
+
+            leftNode!!.right = this
+        }
+    }
+
+    protected abstract fun balance(node: Node)
+
+    override fun innerInsert(key: K, value: T): Node{
+        val sizeBeforeIns = size
+        val insertedNode = super.innerInsert(key, value)
+
+        if (sizeBeforeIns != size) {
+            balance(insertedNode)
+        }
+        // If size of the tree is the same
+        // before and after insertion
+        // it means we only change value
+        // one of the nodes without really insertion
+        return insertedNode
+    }
+
+}
