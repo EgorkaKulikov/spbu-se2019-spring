57eb34eb64f02f3bc005fe47aa69ad3935d7e534
Vadim Abzalov <vadim.i.abzalov@gmail.com>
2019-05-12 18:35:36 +0300

    Code cleanup

--- Task05/src/Controller.kt
+++ Task05/src/Controller.kt
@@ -4,25 +4,19 @@ import kotlin.system.exitProcess
 
 class Controller(args: Array<String>) {
 
-    private var archiveName: String
-
     init {
-        if (args.size < 2 ||
-            !(args[0].endsWith(".zip")) ||
-            args[1] !in listOf("show-all", "show-file", "show-folder") ||
-            (args[1] == "show-all" && args.size != 2) ||
-            (args[1] != "show-all" && args.size != 3) ||
-            args.size > 3
-        ) printUsage()
-        archiveName = args[0]
-        when (args[1]) {
-            "show-all" -> View(archiveName).printArchive()
-            "show-file" -> View(archiveName).printFile(args[2])
-            "show-folder" -> View(archiveName).printFolder(args[2])
+
+        when {
+            args.size == 2 && args[1] == "show-all" -> View(args[0]).printArchive()
+            args.size == 3 && args[1] == "show-file" -> View(args[0]).printFile(args[2])
+            args.size == 3 && args[1] == "show-folder" -> View(args[0]).printFolder(args[2])
+            else -> printUsage()
         }
+
     }
 
     private fun printUsage() {
+
         println(
             """
             |Type one of the following commands:
@@ -32,6 +26,7 @@ class Controller(args: Array<String>) {
         """.trimMargin()
         )
         exitProcess(127)
+
     }
 
 }
--- Task05/src/Model.kt
+++ Task05/src/Model.kt
@@ -10,36 +10,46 @@ class Model(archiveName: String) {
     private var file: ZipFile
 
     init {
+
         try {
             file = ZipFile(archiveName)
         } catch (ex: Exception) {
             println("In Model:\n\t$ex")
             exitProcess(2)
         }
+
     }
 
     private fun findByName(name: String): Array<ZipEntry> {
+
         val result: MutableList<ZipEntry> = mutableListOf()
         val enum = file.entries()
+
         while (enum.hasMoreElements()) {
             val curEntry = enum.nextElement() as ZipEntry
             if (curEntry.name.endsWith(name)) {
                 result.add(curEntry)
             }
         }
+
         return result.toTypedArray()
+
     }
 
     fun getFolderSize(folderName: String): Long {
+
         var folderSize: Long = 0
         val enum = file.entries()
+
         while (enum.hasMoreElements()) {
             val curEntry = enum.nextElement() as ZipEntry
             if (curEntry.name.startsWith(folderName)) {
                 folderSize += curEntry.size
             }
         }
+
         return folderSize
+
     }
 
     fun getFileCreationDate(fileName: String): LocalDateTime = findByName(fileName)[0].timeLocal
