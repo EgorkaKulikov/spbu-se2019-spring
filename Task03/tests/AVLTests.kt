26e19f89981ec683985c4d6048c195e6d8f63205
GordienkoEgor <43183357+GordienkoEgor@users.noreply.github.com>
2019-05-18 20:56:34 +0300

    Update AVLTests.kt

--- Task03/tests/AVLTests.kt
+++ Task03/tests/AVLTests.kt
@@ -1,6 +1,7 @@
 package trees
 
 import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertNotEquals
 import org.junit.jupiter.api.Test
 import kotlin.random.Random
 
