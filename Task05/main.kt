ef67f6240fbb981933df079b5a423ddd09b77e91
Kuzivanov Sergey <43182277+SergeyKuz1001@users.noreply.github.com>
2019-05-23 21:48:06 +0300

    Update main.kt

--- Task05/main.kt
+++ Task05/main.kt
@@ -1,25 +1,10 @@
-const val pathJava = "..."
-const val pathIntelliJ = "..."
-const val pathProduction = "..."
-const val pathKotlinLib = "..."
-//вместо насоящих путей до java.exe и т. д. пока поставлены многоточия, чтобы проект
-//заработал, необходимо пока прописывать пути вручную
+import view.View
 
+@kotlin.ExperimentalUnsignedTypes
 fun main(args: Array<String>) {
     val argsViewProcess = if (args.isEmpty())
-        arrayOf()
+        arrayOf("-createNewWindow")
     else
-        arrayOf("-openArchive") + args
-    ProcessBuilder(
-        "cmd", "/c",
-        "start $pathJava \"-javaagent:$pathIntelliJ\\lib\\idea_rt.jar=61388:" +
-                "$pathIntelliJ\\bin\" -Dfile.encoding=UTF-8 " +
-                "-classpath $pathProduction;" +
-                "$pathKotlinLib\\kotlin-stdlib.jar;" +
-                "$pathKotlinLib\\kotlin-reflect.jar;" +
-                "$pathKotlinLib\\kotlin-test.jar;" +
-                "$pathKotlinLib\\kotlin-stdlib-jdk7.jar;" +
-                "$pathKotlinLib\\kotlin-stdlib-jdk8.jar view.ViewProcessKt " +
-                argsViewProcess.joinToString(separator = "\" \"", prefix = "\"", postfix = "\"")
-    ).start()
+        arrayOf("-openNewWindow -openArchive") + args
+    View.execute(argsViewProcess.toList(), null)
 }
